83b34f1e08572d0f4c3a3314c84553cb2bf410c9
zeroKYH <koreaguh@naver.com>
2024-07-13 15:32:07 +0900

    디자인 추가

--- src/SideMenubar.tsx
+++ src/SideMenubar.tsx
@@ -1,4 +1,3 @@
-import { Button, ButtonGroup } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 const SideMenuBar = () => {
@@ -6,11 +5,9 @@ const SideMenuBar = () => {
 
   return (
     <div className="flex flex-col px-5 py-1 w-40 h-svh text-sm justify-evenly bg-sidebarColor text-white">
-      <ButtonGroup variant="text" aria-label="Basic button">
-        <Button onClick={() => navigate("/PassiveCheck")}>의석님</Button>
-        <Button onClick={() => navigate("/AttendanceDashboard")}>도은님</Button>
-        <Button onClick={() => navigate("/Consulting")}>윤호님</Button>
-      </ButtonGroup>
+      <button onClick={() => navigate("/PassiveCheck")}>의석님</button>
+      <button onClick={() => navigate("/AttendanceDashboard")}>도은님</button>
+      <button onClick={() => navigate("/Consulting")}>윤호님</button>
     </div>
   );
 };
